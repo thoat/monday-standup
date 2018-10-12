e444094a0983a504572094259383230138c19612
Thoa T <thoat@users.noreply.github.com>
2018-10-11 23:18:14 -0400

    Improve the data structures used in pairing
    Eliminate the use of max heap

--- src/helper-pairing.js
+++ src/helper-pairing.js
@@ -14,17 +14,17 @@ OUTPUT: an array of pairs. Format like below:
 
 Steps:
 === A: Prepare the Foundations ===
-1. Create a max-heap of on-site team sizes. Each heap node stores {name: teamName, value: teamSize}.
+1. Iterate through the array of members to create a hash of on-site team sizes. {key: teamName, value: teamSize}.
 
-2. Create a hash from this heap: {key: teamName, value: i} with i being an incrementing index. What that means: the first teamName to be popped from the heap will have an i=0, the next teamName i=1, and so on.
+2. Turn this hash into an array via Object.entries() and sort it from max to min team sizes. Then use the indices to build a teamIndex hash. Biggest team corresponds to value 0, and so on.
 
-3. Start building a (sorted) array of stacks. Each stack represents a team. How?
+3. Start building a (sorted) array of array. Each subarray represents a team. How?
   3a. Iterate through the memberArray
-  3b. If the member is not absent, look up the member's teamName in the Hash created at step 2 in order to push the member to the correct stack.
-    let stacks = []
+  3b. If the member is not absent, look up the member's teamName in the Hash created at step 2 in order to push the member to the correct subarray.
+    let teams = []
     memberArray.forEach(member => {
       if (!member.isAbsent)
-        stacks[hashtbl.get(member.teamName)].push(member)
+        teams[teamIndex[member.teamName]].push(member)
     })
 
 === B: Pair Randomly ===
@@ -32,35 +32,31 @@ Steps:
     const INITIAL_PIVOT = 0 (the index of the biggest-sized team)
     const INITIAL_HOP = a random number between 0 and no. of teams
     let pivot = 0
-    let pivotStack = stacks[pivot]
+    let pivotTeam = teams[pivot]
     let partner = pivot + INITIAL_HOP
 2.
   Loop k times, with k being half the number of members present.
   pivot is where we get card1. partner is where we get card2.
   In each iteration:
-    while pivotStack is empty {
+    while pivotTeam is empty {
       keep incrementing pivot
     }
-    let card1 = pivotStack.pop()
+    let card1 = pivotTeam.pop()
     increment partner, unless this is the very first loop
-    while (partnerStack is empty && partner !== INITIAL_PIVOT) {
-      keep incrementing partner to reach a non-empty stack
+    while (partnerTeam is empty && partner !== INITIAL_PIVOT) {
+      keep incrementing partner to reach a non-empty team
     }
     if partner reaches the INITIAL_PIVOT, skip to the current pivot plus 1
-    let card2 = partnerStack.pop()
+    let card2 = partnerTeam.pop()
     save {card1: card1, card2: card2}
 */
 
-import MaxHeap from "collections/heap"
-import Stack from "collections/shim-array"
-Stack.prototype.peek = Stack.prototype.peekBack // override this method to make this data structure resemble a traditional stack
+import Array from "collections/shim-array" // use this library to utilize its sorted() and peekBack() functions
 
 export function yieldThePairs(memberList) {
   const MEMBERS = memberList
 
-// === build the heap ===
-
-// first, count the number of members in each team and store those into a hash
+// build the hash of on-site team sizes
   let numPresentMembers = 0
   let teamSizeHash = MEMBERS.reduce((h, member) => {
     if (!member.isAbsent) {
@@ -73,63 +69,54 @@ export function yieldThePairs(memberList) {
     return h
   }, {})
 
-// push the size values into the heap
-  let teamSizeHeap = new MaxHeap(
-    Object.entries(teamSizeHash),
-    null,
-    (teamA, teamB) => teamA[1] - teamB[1] // index 0 stores the team name, index 1 stores the team size
-  )
+// sort team sizes
+  let teamIndex =
+    new Array(...Object.entries(teamSizeHash))
+    .sorted((a, b) => b[1] - a[1]) // index 0 stores the team name, index 1 stores the team size; b-a cuz we want to sort decreasingly
+    .reduce((hash, elem, idx) => {
+      hash[elem[0]] = idx // {key: team name, value: index in the sizeArray}
+      return hash
+    }, {})
 
-// === build the array of teamStacks ===
-
-// first, obtain the index i.e. where each team should be located in the array
-  let teamIndexHash = {}
-  let i = 0
-  while (teamSizeHeap.length) { // while heap is not empty
-    let team = teamSizeHeap.pop()
-    teamIndexHash[team[0]] = i
-    i++
-  }
-
-// build the stacks and put them in the correct array index
-  let stacks = []
+// build the arrays of teams
+  let teams = []
   MEMBERS.forEach(member => {
     if (!member.isAbsent) {
       let teamName = member.team
-      if (!stacks[teamIndexHash[teamName]])
-        stacks[teamIndexHash[teamName]] = new Stack()
-      stacks[teamIndexHash[teamName]].push(member)
+      if (!teams[teamIndex[teamName]])
+        teams[teamIndex[teamName]] = new Array()
+      teams[teamIndex[teamName]].push(member)
     }
   })
 
-// === pairing ===
+// pairing
   const INITIAL_PIVOT = 0 // the index of the biggest-sized team
-  const INITIAL_HOP = Math.floor(Math.random() * stacks.length)
+  const INITIAL_HOP = Math.floor(Math.random() * teams.length)
   let pivot = INITIAL_PIVOT
-  let pivotStack = stacks[pivot]
+  let pivotTeam = teams[pivot]
   let partner = pivot + INITIAL_HOP
 
   let listOfPairs = []
   for (let i = 0; i < numPresentMembers/2; i++) {
-    while (!pivotStack.peek()) {
+    while (!pivotTeam.peekBack()) {
       pivot++
-      pivotStack = stacks[pivot]
+      pivotTeam = teams[pivot]
     }
-    let card1 = pivotStack.pop()
-    partner = i === 0 ? partner : (partner + 1) % stacks.length
-    let partnerStack = stacks[partner]
-    while (!partnerStack.peek() && partner !== INITIAL_PIVOT) {
-      partner = (partner + 1) % stacks.length // keep incrementing to reach a non-empty stack
-      partnerStack = stacks[partner]
+    let card1 = pivotTeam.pop()
+    partner = i === 0 ? partner : (partner + 1) % teams.length
+    let partnerTeam = teams[partner]
+    while (!partnerTeam.peekBack() && partner !== INITIAL_PIVOT) {
+      partner = (partner + 1) % teams.length // keep incrementing to reach a non-empty team
+      partnerTeam = teams[partner]
     }
     if (partner === INITIAL_PIVOT) { // if reaching the INITIAL_PIVOT, skip to the current pivot plus 1
       partner = pivot + 1
-      partnerStack = stacks[partner]
+      partnerTeam = teams[partner]
     }
-    let card2 = partnerStack.pop()
+    let card2 = partnerTeam.pop()
     listOfPairs.push({card1: card1, card2: card2})
     // console.log(i, {card1: card1, card2: card2})
   }
-  console.log(listOfPairs)
+  // console.log(listOfPairs)
   return listOfPairs
 }
