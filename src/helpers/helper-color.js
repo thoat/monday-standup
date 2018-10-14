806aed1d24afc245d31d7c7581b667a7440438d7
Thoa T <thoat@users.noreply.github.com>
2018-10-14 09:00:06 -0400

    Refactor team's names and colors to config

--- src/helpers/helper-color.js
+++ src/helpers/helper-color.js
@@ -1,30 +1,17 @@
+import config from '../config'
+
 export function getCardColors(team) {
   switch(team) {
-    case "Science": return COLOR_DS
-    case "Dev": return COLOR_DEV
-    case "Design": return COLOR_DESIGN
-    case "Comms": return COLOR_COMMS
-    case "Accessibility": return COLOR_ACCSS
-    case "Partnerships": return COLOR_PARTNER
-    case "Blockchain": return COLOR_BLKCH
-    case "Fund": return COLOR_FUND
-    case "VR/Events": return COLOR_VR
-    case "Others/Visitors": return COLOR_OTHER
-    default: return COLOR_ABSENT
+    case config.TEAMSTR_DS: return config.COLOR_DS
+    case config.TEAMSTR_DEV: return config.COLOR_DEV
+    case config.TEAMSTR_DESIGN: return config.COLOR_DESIGN
+    case config.TEAMSTR_COMMS: return config.COLOR_COMMS
+    case config.TEAMSTR_ACCSS: return config.COLOR_ACCSS
+    case config.TEAMSTR_PARTNER: return config.COLOR_PARTNER
+    case config.TEAMSTR_BLKCH: return config.COLOR_BLKCH
+    case config.TEAMSTR_FUND: return config.COLOR_FUND
+    case config.TEAMSTR_VR: return config.COLOR_VR
+    case config.TEAMSTR_OTHER: return config.COLOR_OTHER
+    default: return config.COLOR_ABSENT
   }
 }
-
-const COLOR_DS = ["#ffcccc", "#ff9999"] // rose
-const COLOR_DEV = ["#99ccff", "#0066cc"] // blue
-const COLOR_DESIGN = ["#ff99cc", "#990099"] // fuschia
-const COLOR_COMMS = ["#ffff99", "#999900"] // yellow
-const COLOR_ACCSS = ["#ff6699", "#cc0033"] // red
-const COLOR_PARTNER = ["#ffcc99", "#ff9900"] // orange
-const COLOR_BLKCH = ["#99ffcc", "#009966"] // lime green
-const COLOR_FUND = ["#ccccff", "#663399"] // lavender
-const COLOR_VR = ["#99ff33", "#66cc00"] // green
-const COLOR_OTHER = ["#dddddd", "#dddddd"] // silver
-const COLOR_ABSENT = ["#999999", "#999999"] // grey
-
-// Other great colors if needed:
-// const COLOR_ = ["#9edfdf", "#339999"] // soft cyan
